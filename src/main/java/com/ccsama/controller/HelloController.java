5ff692c7e55039ae297ce3fd1641b9f8101045aa
AVICCII <863134471@qq.com>
2020-08-11 13:42:28 +0800

    test_without_gui

---
+++ src/main/java/com/ccsama/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.ccsama.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author aviccii 2020/8/10
+ *
+ *
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String Hello(){
+    return "hello,ccsama";
+    }
+}
